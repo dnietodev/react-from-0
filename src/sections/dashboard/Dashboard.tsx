0da41fe4becb97781c12183826c7575171576f40
Daniel Nieto <dnieto@hiberus.com>
2023-04-10 12:24:38 +0200

    ADD loops, react fragment

--- src/sections/dashboard/Dashboard.tsx
+++ src/sections/dashboard/Dashboard.tsx
@@ -1,3 +1,16 @@
+import { config } from "../../devdash_config";
+
 export function Dashboard() {
-    return <div>Dashboard</div>
+  const title = "DevDash";
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <ul>
+        {config.widgets.map((widget) => (
+          <li key={widget.id}>{widget.repository_url}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
